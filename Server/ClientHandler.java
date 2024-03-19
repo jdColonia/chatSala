5bf098aa6507bb2bc78dfc4af7a5dd95be339591
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-03-19 09:49:02 -0500

    Funcionalidad para añadir un nuevo usuario al chat

--- Server/ClientHandler.java
+++ Server/ClientHandler.java
@@ -6,11 +6,13 @@ import java.util.*;
 //agrega el usuario y su canal de comunicacion a la lista de chatters
 //permite enviar mensajes a todos los usuarios
 class ClientHandler implements Runnable {
-    private Socket clientSocket;
-    private BufferedReader in;
-    private PrintWriter out;
-    private String clientName;
-    Chatters clientes;
+    
+    private Socket clientSocket; // Socket para la conexión con el cliente
+    private BufferedReader in; // Flujo de entrada para leer los mensajes del cliente
+    private PrintWriter out; // Flujo de salida para enviar mensajes al cliente
+    private String clientName; // Nombre de usuario del cliente
+    Chatters clientes; // Objeto que contiene la lista de clientes conectados
+
     public ClientHandler(Socket socket,Chatters clientes) {
         //asignar los objetos que llegan a su respectivo atributo en la clase
         //crear canales de entrada in y de salida out para la comunicacion
@@ -19,13 +21,21 @@ class ClientHandler implements Runnable {
     @Override
     public void run() {
         String message;
-        //implementar la logica que permita soliciar a un cliente un nombre de usuario 
-        //verificar que no exista en chatters
-        //notificar a los demas clientes que un nuevo usuario se ha unido
-        //agregar al nuevo usuario a chatters junto con su canal de salida out
-        //notificar al cliente que ha sido aceptado
 
-        //ante un nuevo mensaje de ese cliente, enviar el mensaje a todos los usuarios
-        
+        try {
+            clientName = in.readLine(); // Solicita un nombre de usuario a un cliente
+            while (clientes.personExist(clientName)) { // Verifica que el nombre de usuario del nuevo cliente no exista
+                out.println("Nombre de usuario ya existente. Por favor, ingrese otro nombre."); // Solicita de nuevo el nombre si ese nombre ya está en uso
+                clientName = in.readLine();
+            }
+            clientes.addPerson(clientName, out); // Añade al cliente al chatters con su canal de salida out
+            clientes.sendMessageToAll(clientName + " se ha unido al chat."); // Notifica a los demás usuarios que hay un nuevo miembro en el chat
+            out.println("Nombre aceptado"); // Notifica al cliente que fue aceptado
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 }
